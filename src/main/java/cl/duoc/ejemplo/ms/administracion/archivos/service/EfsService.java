7e215e576a49e61ab56d5991e7dd31d87723ef07
LeoOlivaresD <cspraword@gmail.com>
2025-06-24 23:46:13 -0400

    Se refatoriza metodo save.efs porque depende de que el archivo temporal aún exista en el sistema de archivos, pero tomcat puede limpiar ese archivo antes de que se ejecute, especialmente si otra parte del código ya accedió al stream

--- src/main/java/cl/duoc/ejemplo/ms/administracion/archivos/service/EfsService.java
+++ src/main/java/cl/duoc/ejemplo/ms/administracion/archivos/service/EfsService.java
@@ -2,6 +2,9 @@ package cl.duoc.ejemplo.ms.administracion.archivos.service;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -13,14 +16,15 @@ public class EfsService {
     @Value("${efs.path}")
     private String efsPath;
 
-    public File saveToEfs(String filename, MultipartFile multipartFile) throws IOException {
-
-        File dest = new File(efsPath, filename);
-        File parentDir = dest.getParentFile();
-        if (parentDir != null && !parentDir.exists()) {
-            parentDir.mkdirs();
-        }
-        multipartFile.transferTo(dest);
-        return dest;
+    public void saveToEfs(String filename, MultipartFile multipartFile) {
+    try {
+        Path destino = Path.of(efsPath, filename);
+        Files.createDirectories(destino.getParent()); // por si no existe la carpeta
+        Files.copy(multipartFile.getInputStream(), destino, StandardCopyOption.REPLACE_EXISTING);
+        System.out.println("Archivo guardado en EFS: " + destino.toAbsolutePath());
+    } catch (IOException e) {
+        throw new RuntimeException("Error al guardar el archivo en EFS", e);
     }
+}
+
 }
